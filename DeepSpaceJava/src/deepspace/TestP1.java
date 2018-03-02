99484d66b9aca51f8d48fa04cb85dca6957b4f44
Samuelmed <samuelmed.97@gmail.com>
2018-03-02 22:25:06 +0100

    added first test.

---
+++ DeepSpaceJava/src/deepspace/TestP1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deepspace;
+
+/**
+ *
+ * @author samuel
+ */
+public class TestP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Esto es una prueba");
+        
+        // Creo un objeto por cada tipo de clase y uso alguno de sus consultores..
+        Loot Lote = new Loot(1,2,3,4,5);
+        SuppliesPackage
+        
+        System.out.print("El lote tiene las siguientes medallas:" + Lote.getNMedals());
+    }
+    
+}
