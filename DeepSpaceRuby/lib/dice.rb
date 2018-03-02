db0585cdacca1cff3fd84a151ec128a0ba339ba7
laura <sanchezparra@gmail.com>
2018-03-02 16:40:28 +0100

    Finished dice

---
+++ DeepSpaceRuby/lib/dice.rb
@@ -0,0 +1,71 @@
+
+module Deepspace
+  
+  class Dice
+    
+    def initialize
+      
+       @NHANGARSPROB=0.25
+       @NSHIELDSPROB=0.25
+       @NWEAPONSPROB=0.33
+       @FIRSTSHOTPROB=0.5
+       
+       @generator=Random.new()
+
+    end
+    
+    
+   
+    def initWithNHangars
+        if generator.nextDouble()<= NHANGARSPROB 
+            return 0
+        else
+            return 1
+         end
+    end
+    
+    def initWithWeapons 
+      
+       prob=generator.rand(1.0)
+       if prob < NWEAPONSPROB
+           return 1
+       elsif prob>NWEAPONSPROB && prob<(2*NWEAPONSPROB)
+           return 2
+       else
+           return 3
+       end
+    end
+    
+    def initWithShields
+        if generator.rand(1.0)< NSHIELDSPROB
+          return 0
+        else
+            return 1
+        end
+    end
+    
+    def whoStarts(nPlayers)
+      
+        generator.rand(nPlayers)
+    end
+    
+    def firstShot
+      #spacestation=GameCharacter.new.
+      #enemystarship=GameCharacter.new
+     
+         if generator.rand(1.0)<FIRSTSHOTPROB
+             return spacestation
+         else
+             return enemystarship
+         end
+    end
+    
+    def spaceStationMoves(speed)
+         if generator.rand(1.0)<speed
+             return true
+         else
+             return false
+         end
+    end
+  end
+end
--- DeepSpaceRuby/lib/weapon_type.rb
+++ DeepSpaceRuby/lib/weapon_type.rb
@@ -19,7 +19,7 @@ module DeepSpace
 
 
       LASER=Type.new(2.0)
-      MISSILE =Type.new(3.0)
-      PLASMA =Type.new(4.0)
+      MISSILE=Type.new(3.0)
+      PLASMA=Type.new(4.0)
   end
 end
