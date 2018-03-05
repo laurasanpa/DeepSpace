ea4475c4c2ae757236199bd6b8e52ad1f5c95552
Samuelmed <samuelmed.97@gmail.com>
2018-03-05 18:23:23 +0100

    nothing new

--- DeepSpaceRuby/lib/test_p1.rb
+++ DeepSpaceRuby/lib/test_p1.rb
@@ -1,19 +1,40 @@
 # encoding: utf-8
 
-module Deepspace
+require 'loot'
+require_relative "supplies_package"
 class TestP1
+
     def main()
     
-    puts 'Esto es una prueba'
+      puts 'Esto es una prueba'
     
-    #botín.
-    lote = Loot.new(1.0,2.0,3.0,4.0,5.0)
-    puts "El botín tiene las siguientes medallas  #{lote.nMedals}"
-  end
+      #botín.
+      lote= Loot.new(1,2,3,4,5)
+      puts "El botín tiene las siguientes medallas  #{lote.nMedals}"
+      
+      
+      #Paquetes de suministros
+      paq= SuppliesPackage.new(2,1,1)
+      puts "El paquete de suministros tiene la siguiente cantidad de suministro #{paq.fuelUnits}"
+      puts "El paquete de suministros tiene la siguiente recarga de escudos: #{paq.shieldpower}"
+      
+      # shield booster
+      nombre = "pepe"
+      
+      recarga = ShieldBooster.new(nombre,2,2)
+      
+      puts "Usos del potenciador de escudos: #{recarga.uses}" 
+      puts "useIt: #{recarga.useIt}"
+     
+      #weapon
+      #No estoy seguro de como asignar laser y como consultar el tipo de arma.
+      nombre1 = "lolita"
+      arma1 = WeaponType::LASER
+      puts "Tipo de arma: #{}"
+    end
   
-    c=TestP1.new
+
+end
+
+      c=TestP1.new
     c.main
-end
-
-
-end
